bcbb6e63107c041397e9d409f1c5b2f877c52f7f
sushil <bxrsushil@gmail.com>
2023-06-09 15:23:34 +0530

    sk

--- app/src/main/java/com/app/smarticondistributor/NavigationActivity.kt
+++ app/src/main/java/com/app/smarticondistributor/NavigationActivity.kt
@@ -1,11 +1,69 @@
 package com.app.smarticondistributor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.app.smarticondistributor.adapters.DailyTranscationAdapters
+import com.app.smarticondistributor.adapters.OrderDetailsAdapters
+import com.app.smarticondistributor.adapters.ProductAdapters
+import com.app.smarticondistributor.databinding.ActivityNavigationBinding
 
 class NavigationActivity : AppCompatActivity() {
+    private lateinit var b: ActivityNavigationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_navigation)
+        b = ActivityNavigationBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+        b.back.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+               onBackPressed()
+            }
+        })
+        b.dashboard.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, DashboardActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.selling.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, SellActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.order.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, OrderManagementActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.deposite.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, DepositeRequestActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.wallet.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, WalletActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.profile.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, ProfileActivity::class.java);
+                startActivity(intent);
+            }
+        })
+        b.orderList.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@NavigationActivity, OrderActivity::class.java);
+                startActivity(intent);
+            }
+        })
+
+
     }
 }
--- app/src/main/java/com/app/smarticondistributor/OrderActivity.kt
+++ app/src/main/java/com/app/smarticondistributor/OrderActivity.kt
@@ -2,10 +2,26 @@ package com.app.smarticondistributor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.app.smarticondistributor.adapters.OrderDetailsAdapters
+import com.app.smarticondistributor.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
+    private lateinit var b: ActivityOrderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        b = ActivityOrderBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+        b.back.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+              onBackPressed()
+            }
+        })
+
+        val orderDetailsAdapters = OrderDetailsAdapters(this)
+        b.rvOrder.adapter = orderDetailsAdapters
+
+
     }
 }
