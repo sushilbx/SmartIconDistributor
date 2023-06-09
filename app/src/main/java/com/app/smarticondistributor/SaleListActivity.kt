98d21e3082f4db0ac01c6fa8445e66f25b50e23c
sushil <bxrsushil@gmail.com>
2023-06-09 16:23:05 +0530

    sk

--- app/src/main/java/com/app/smarticondistributor/SaleListActivity.kt
+++ app/src/main/java/com/app/smarticondistributor/SaleListActivity.kt
@@ -2,10 +2,25 @@ package com.app.smarticondistributor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.app.smarticondistributor.adapters.SellListAdapters
+import com.app.smarticondistributor.databinding.ActivitySaleListBinding
 
 class SaleListActivity : AppCompatActivity() {
+    private lateinit var b: ActivitySaleListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sale_list)
+        b = ActivitySaleListBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+        b.back.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+               onBackPressed()
+            }
+        })
+        val sellListAdapters = SellListAdapters(this)
+        b.rvSelling.adapter = sellListAdapters
+
+
     }
 }
--- app/src/main/java/com/app/smarticondistributor/SellActivity.kt
+++ app/src/main/java/com/app/smarticondistributor/SellActivity.kt
@@ -1,11 +1,23 @@
 package com.app.smarticondistributor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.app.smarticondistributor.databinding.ActivitySellBinding
 
 class SellActivity : AppCompatActivity() {
+    private lateinit var b: ActivitySellBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sell)
+        b = ActivitySellBinding.inflate(layoutInflater)
+        val view = b.root
+        setContentView(view)
+        b.mbSell.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val intent = Intent(this@SellActivity, SaleListActivity::class.java);
+                startActivity(intent);
+            }
+        })
     }
 }
